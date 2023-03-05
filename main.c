4111ddcdc1a2899e4dceb5090e59a3696b35d534
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-04 23:06:20 -0600

    added values to malloced array

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc/malloc.h>
+//#include <malloc/malloc.h>
 
 int main() {
-  int x;
+  int x, y, z;
   int uInput;
   char *arrPointer;
   int arrLength;
+  char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   printf("Enter the number of values:");
   scanf("%d", &uInput);
@@ -16,20 +17,45 @@ int main() {
 
   arrPointer = malloc(uInput);
 
-  printf("Size of new array is: %i\n", uInput);
-  
-  *(arrPointer+2) = 7;
-  
- 
+  printf("Size of new array is: %i\n\n", uInput);
+
+
+
+  /*
+  Examle of seeting values after a malloc
+
+  *(arrPointer+2) = 7; 
   printf("Value of 3rd position is: %d\n", *(arrPointer+2));
- 
+
+  */ 
+  
   // cannot do this with a malloc
   // this only works with an actual array you initilized
   // printf("Size of total array is: %lu", sizeof(*arrPointer));
   // arrLength = sizeof(*arrPointer)/sizeof(*(arrPointer+1));
   // have to add another library depending on the OS you are on.
+  // You have to add <malloc/malloc.h> for Mac OS.
+  // printf("Value of Malloced array is: %lu\n", malloc_size(arrPointer));
 
-  printf("Value of Malloced array is: %lu\n", malloc_size(arrPointer));
+
+
+  for (x=0; x<uInput; x++) {
+    arrPointer[x] = alphabet[x];
+  }
+
+  for (z=0; z<uInput; z++) {
+    printf("arrPointer[%d]=%c\n", z, arrPointer[z]);
+  }
+
+
+
+
+
+
+
+
+
+
 
   return 0;
 }
