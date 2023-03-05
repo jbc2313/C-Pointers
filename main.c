cec97922d393e17d09a3cb5d433c0169473d5291
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-04 21:53:52 -0600

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+
+#include "stdio.h"
+
+
+
+
+
+int main() {
+
+  int uInput;
+  char *arrPointer;
+
+  printf("Enter the number of values:");
+  scanf("%d", &uInput);
+
+  printf("user input was: %d\n", uInput);
+
+
+  return 0;
+}
